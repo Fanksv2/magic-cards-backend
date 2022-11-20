435c3de09cb08017b60ef0acc11710cbd6f2395b
Gabriel Arruda <gabriel.arruda@oktagon.com.br>
2022-11-20 16:02:24 -0300

    new: router configurations and add mongoose

---
+++ control/router.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var router = express.Router();
+var auth = require("./routes/auth");
+
+router.use("/auth/", auth);
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const dotenv = require("dotenv");
+const cors = require("cors");
+const router = require("./control/router");
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+dotenv.config();
 
+const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use("/", router);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Port: ${port}`);
 });
